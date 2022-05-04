4d4d196de4464e5cad4e0979a62800f9ea5369fb
Hao <xpdinghao@163.com>
2022-05-04 23:48:00 +0200

    mpi finish first version

--- DFT.cpp
+++ DFT.cpp
@@ -4,13 +4,14 @@
 #include <vector>
 #include <memory>
 
-// #include <mpi.h>
+#include <mpi.h>
 
 #include "lodepng.h"
 
 #ifndef M_PI
 constexpr auto  M_PI = 3.14159265358979323846;
 #endif
+#define rootRank 0
 
 void loadPNG(std::string filename, unsigned& w, unsigned& h, std::vector<unsigned char>& image)
 {
@@ -87,6 +88,38 @@ void DFT(std::vector<std::complex<double>>& in, std::vector<std::complex<double>
 	}
 }
 
+void DFT_partial(std::vector<std::complex<double>>& in, std::vector<std::complex<double>>& out, unsigned w, unsigned h, unsigned offset, unsigned lenCount, bool horizontal, bool inverse)
+{
+	out.clear();
+	out.resize(in.size());
+
+	#pragma omp parallel for
+	// for (unsigned i = horizontal ? offset : 0; i < (horizontal ? offset+lenCount : h); ++i)
+	for (unsigned i = offset; i < offset+lenCount; ++i)
+	{
+		for (unsigned k = 0 ; k < w ; ++k)
+		{
+			std::complex<double> sum(0.0, 0.0);
+			for (unsigned j = 0; j < w; ++j)
+			{
+				size_t addr = horizontal ? j + i * w : i + j * w;
+				auto angle = (inverse ? -2.0f : 2.0) * M_PI * j * k / w;
+				sum.real(sum.real() + in[addr].real() * cos(angle) - in[addr].imag() * sin(angle));
+				sum.imag(sum.imag() + in[addr].real() * sin(angle) + in[addr].imag() * cos(angle));
+			}
+
+			if (!inverse)
+			{
+				sum *= 1.0 / w;
+			}
+
+			size_t addr = horizontal ? k + i * w : i + k * w;
+			out[k + i * w] = sum;
+		}
+		// std::cout << i << std::endl;
+	}
+}
+
 void Filter(std::vector<std::complex<double>>& in, int radiusMin, int radiusMax, unsigned w, unsigned h)
 {
 	for (int x = radiusMin; x < radiusMax; ++x)
@@ -113,8 +146,14 @@ void imageNormalization(std::vector<unsigned char>& buffer, std::vector<unsigned
 	}
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+	MPI_Init(&argc, &argv);
+    int size;
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
 	std::string inName("bmei.png");
 	std::string dftName("bmeiDFT.png");
 	std::string dftFName("bmeiDFTF.png");
@@ -127,34 +166,133 @@ int main()
 
 	std::vector<std::complex<double>> f1;
 	std::vector<std::complex<double>> f2;
+	std::vector<std::complex<double>> g_horizontal;
+	std::vector<std::complex<double>> g_image;
+	g_horizontal.clear();
+	g_image.clear();
+	g_horizontal.resize(w*h);
+	g_image.resize(w*h);
+
 	RealToComplex(image, f1);
-	DFT(f1, f2, w, h, true, false);
-	DFT(f2, f1, w, h, false, false);
-	std::cout << "DFT finished" << std::endl;
-	ComplexToReal(f1, image);
-	savePNG(dftName, w, h, image);
+
+	int div=w/size;
+	int mod=w%size;
+	std::vector<int> g_offset;
+	std::vector<int> g_counts;
+	for (size_t i = 0; i < mod; i++)
+	{
+		g_offset.push_back(i*(div+1)*w);
+		g_counts.push_back((div+1)*w);
+	}
+
+	for (size_t i = mod; i < size; i++)
+	{
+		g_offset.push_back((mod*(div+1)+(i-mod)*div)*w);
+		g_counts.push_back(div*w);
+	}
 	
-	std::vector<unsigned char> image_norm;
-	imageNormalization(image,image_norm,w*h);
-	savePNG("test.png", w, h, image_norm);
+	int l_offset, l_counts;
+	if (my_rank<mod)
+	{
+		l_offset=my_rank*(div-1);
+		l_counts=(div+1);
+	}else
+	{
+		l_offset=(mod*(div+1)+(my_rank-mod)*div);
+		l_counts=div;
+	}
 
-	printcheck(image_norm, 30);
+	if (my_rank==rootRank)
+	{
+		for (size_t i = 0; i < size; i++)
+		{
+			printf(" %d", g_offset[i]);
+		}
+		printf("\n");
+		for (size_t i = 0; i < size; i++)
+		{
+			printf(" %d", g_counts[i]);
+		}
+		printf("\n");
+		fflush(stdout);
+	}
+	
 
-	// Frequency filtering
-	int minFreq = 0;
-	int maxFreq = 0;
-	Filter(f1, minFreq, maxFreq, w, h);
-	std::cout << "Filtering finished" << std::endl;
-	ComplexToReal(f1, image);
-	savePNG(dftFName, w, h, image);
+	// DFT(f1, f2, w, h, true, false);
+	DFT_partial(f1, f2, w, h,l_offset, l_counts, true, false);
+	const void *l_send_pointer=&f2[l_offset*w];
+	int l_send_counts=l_counts*w;
 
-	DFT(f1, f2, w, h, true, true);
-	DFT(f2, f1, w, h, false, true);
-	std::cout << "IDFT finished" << std::endl;
-	ComplexToReal(f1, image);
-	savePNG(outName, w, h, image);
+	if (my_rank==rootRank)
+	{
+		printf("DFT Done!");
+		printf("\n");
+		fflush(stdout);
+	}
 
-	printcheck(image, 30);
+	MPI_Gatherv(l_send_pointer,l_send_counts,MPI_DOUBLE_COMPLEX,&g_horizontal[0],&g_counts[0],&g_offset[0],MPI_DOUBLE_COMPLEX,rootRank,MPI_COMM_WORLD);
+	if (my_rank==rootRank)
+	{
+		DFT(f1, f2, w, h, true, false);
+		for (size_t i = 0; i < w*h; i++)
+		{
+			if (f2[i]!=g_horizontal[i])
+			{
+				printf("Mismatch[%zu]\n",i);
+				return 0;
+			}
+		}
+		
+		printf("Gather1 Done!");
+		printf("\n");
+		fflush(stdout);
+	}
+	MPI_Bcast(&g_horizontal[0],w*h,MPI_DOUBLE_COMPLEX,rootRank,MPI_COMM_WORLD);
+
+	if (my_rank==rootRank)
+	{
+		printf("Broadcast Done!");
+		printf("\n");
+		fflush(stdout);
+	}
+	// DFT(f2, f1, w, h, false, false);
+	DFT_partial(g_horizontal, f2, w, h,l_offset, l_counts, false, false);
+
+	MPI_Gatherv(l_send_pointer,l_send_counts,MPI_DOUBLE_COMPLEX,&f1[0],&g_counts[0],&g_offset[0],MPI_DOUBLE_COMPLEX,rootRank,MPI_COMM_WORLD);
+
+	if (my_rank==rootRank)
+	{
+		DFT(g_horizontal, f2, w, h, false, false);
+
+		for (size_t i = 0; i < w*h; i++)
+		{
+			if (f1[i]!=f2[i])
+			{
+				printf("Mismatch[%zu]\n",i);
+				return 0;
+			}
+		}
+
+		std::cout << "DFT finished" << std::endl;
+		ComplexToReal(g_horizontal, image);
+		savePNG(dftName, w, h, image);
+
+		// Frequency filtering
+		int minFreq = 0;
+		int maxFreq = 0;
+		Filter(f1, minFreq, maxFreq, w, h);
+		std::cout << "Filtering finished" << std::endl;
+		ComplexToReal(f1, image);
+		savePNG(dftFName, w, h, image);
+
+		DFT(f1, f2, w, h, true, true);
+		DFT(f2, f1, w, h, false, true);
+		std::cout << "IDFT finished" << std::endl;
+		ComplexToReal(f1, image);
+		savePNG(outName, w, h, image);
+	}
+
+    MPI_Finalize();
 
 	return 0;
 }
--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-all: dft
+all: dft_mpi
+
+dft_mpi: DFT.cpp lodepng.cpp
+	mpic++ -O3 -std=c++11 -o dft DFT.cpp lodepng.cpp
 
 dft: DFT.cpp lodepng.cpp
 	g++ -O3 -std=c++11 -o dft DFT.cpp lodepng.cpp
